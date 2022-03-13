415182e31ce734f06010a90c395666628ae3f65b
Micael Nussbaumer <micaelnussbaumer@gmail.com>
2022-03-13 19:59:05 +0000

    prefixing

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FlagsmithEngine.MixProject do
   def project do
     [
       app: :flagsmith_engine,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -15,7 +15,6 @@ defmodule FlagsmithEngine.MixProject do
         main: "Flagsmith.Client",
         extras: ["README.md"],
         nest_modules_by_prefix: [
-          Flagsmith.Schemas,
           Flagsmith.Schemas.Environment,
           Flagsmith.Schemas.Features,
           Flagsmith.Schemas.Segments,
